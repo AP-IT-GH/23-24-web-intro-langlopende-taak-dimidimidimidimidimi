2f7974550f9a8b5a660324073dab2f007065e508
dimidimidimidimidimi <150671574+dimidimidimidimidimi@users.noreply.github.com>
2024-05-10 21:28:43 +0200

    Deelopdracht4_V1.9
    Tussentijdse commit.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,10 @@
-let map = L.map('map').setView([52.518406573793335, 13.407272201582815], 15);
+let map = L.map('map').setView([52.5200675543843, 13.40481846661213], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 35,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.marker([52.5200675543843, 13.40481846661213]).addTo(map)
+    .bindPopup('Berlijn, de hoofdstad van Duitsland.')
+    .openPopup();
